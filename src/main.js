eb63b1f8ea9e83632b739324859827e5c04d3088
arnau <arnau.udina@gmail.com>
2016-01-27 17:29:09 +0100

    Afegeixo l'hover de les barres i el reservori

---
+++ src/main.js
@@ -0,0 +1,79 @@
+
+d3.selectAll('.Ebola').on('mouseover',function(){
+  d3.selectAll('.Ebola').classed('highlight',true);
+  d3.select('#reserv_murci').classed('Ebola',true);
+});
+d3.selectAll('.Marburgo').on('mouseover',function(){
+  d3.selectAll('.Marburgo').classed('highlight',true);
+  d3.select('#reserv_murci').classed('Marburgo',true);
+});
+d3.selectAll('.Lassa').on('mouseover',function(){
+  d3.selectAll('.Lassa').classed('highlight',true);
+  d3.select('#reserv_rat').classed('Lassa',true);
+});
+d3.selectAll('.CongoCrimea').on('mouseover',function(){
+  d3.selectAll('.CongoCrimea').classed('highlight',true);
+  d3.select('#reserv_vaca').classed('CongoCrimea',true);
+});
+d3.selectAll('.ValleRift').on('mouseover',function(){
+  d3.selectAll('.ValleRift').classed('highlight',true);
+  d3.select('#reserv_ovella').classed('ValleRift',true);
+});
+d3.selectAll('.MERS').on('mouseover',function(){
+  d3.selectAll('.MERS').classed('highlight',true);
+  d3.select('#reserv_camell').classed('MERS',true);
+  d3.select('#reserv_murci').classed('MERS',true);
+});
+d3.selectAll('.SARS').on('mouseover',function(){
+  d3.selectAll('.SARS').classed('highlight',true);
+  d3.select('#reserv_geneta').classed('SARS',true);
+  d3.select('#reserv_murci').classed('SARS',true);
+});
+d3.selectAll('.Nipah').on('mouseover',function(){
+  d3.selectAll('.Nipah').classed('highlight',true);
+    d3.select('#reserv_murci').classed('Nipah',true);
+});
+d3.selectAll('.Hendra').on('mouseover',function(){
+  d3.selectAll('.Hendra').classed('highlight',true);
+    d3.select('#reserv_murci').classed('Hendra',true);
+});
+
+
+d3.selectAll('.Ebola').on('mouseout',function(){
+  d3.selectAll('.Ebola').classed('highlight',false);
+  d3.select('#reserv_murci').classed('Ebola',false);
+});
+d3.selectAll('.Marburgo').on('mouseout',function(){
+  d3.selectAll('.Marburgo').classed('highlight',false);
+  d3.select('#reserv_murci').classed('Marburgo',false);
+});
+d3.selectAll('.Lassa').on('mouseout',function(){
+  d3.selectAll('.Lassa').classed('highlight',false);
+  d3.select('#reserv_rat').classed('Lassa',false);
+});
+d3.selectAll('.CongoCrimea').on('mouseout',function(){
+  d3.selectAll('.CongoCrimea').classed('highlight',false);
+  d3.select('#reserv_vaca').classed('CongoCrimea',false);
+});
+d3.selectAll('.ValleRift').on('mouseout',function(){
+  d3.selectAll('.ValleRift').classed('highlight',false);
+  d3.select('#reserv_ovella').classed('ValleRift',false);
+});
+d3.selectAll('.MERS').on('mouseout',function(){
+  d3.selectAll('.MERS').classed('highlight',false);
+  d3.select('#reserv_camell').classed('MERS',false);
+  d3.select('#reserv_murci').classed('MERS',false);
+});
+d3.selectAll('.SARS').on('mouseout',function(){
+  d3.selectAll('.SARS').classed('highlight',false);
+  d3.select('#reserv_geneta').classed('SARS',false);
+  d3.select('#reserv_murci').classed('SARS',false);
+});
+d3.selectAll('.Nipah').on('mouseout',function(){
+  d3.selectAll('.Nipah').classed('highlight',false);
+    d3.select('#reserv_murci').classed('Nipah',false);
+});
+d3.selectAll('.Hendra').on('mouseout',function(){
+  d3.selectAll('.Hendra').classed('highlight',false);
+    d3.select('#reserv_murci').classed('Hendra',false);
+});
